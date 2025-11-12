4d9909c6a1abe3b2256417f06a307856a32a9d6d
Ruzan-Mohamed <mohamedruzan.pro@gmail.com>
2025-11-12 17:07:44 +0530

    Update script.js

--- packages/js/script.js
+++ packages/js/script.js
@@ -1,4 +1,18 @@
+// Javascript fo toggling the navbar on small screens 
 function toggleMenu(){
       const menu = document.querySelector('.navbar ul');
       menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+//Close the menu when a link clicked (For Mobiles)
+document.querySelectorAll('.navbar ul li a') .forEach (link => {
+      link.addEventListener('click', () => {
+            const menu = document.querySelector ('.navbar ul');
+            if (window.innerWidth < 768) {
+                  menu.style.display = 'none';
+            }
+            else if (window.innerWidth > 768) {
+                  menu.style.display = 'flex';
+            }
+      })
+});
